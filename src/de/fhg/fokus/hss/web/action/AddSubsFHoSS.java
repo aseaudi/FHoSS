8fe19f03d064647486319ceebacf2ac5818108dd
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2023-05-14 01:35:48 +0300

    add impi

--- src/de/fhg/fokus/hss/web/action/AddSubsFHoSS.java
+++ src/de/fhg/fokus/hss/web/action/AddSubsFHoSS.java
@@ -8,11 +8,28 @@ import de.fhg.fokus.hss.db.model.IMSU;
 import de.fhg.fokus.hss.db.op.IMPI_DAO;
 import de.fhg.fokus.hss.db.op.IMSU_DAO;
 import de.fhg.fokus.hss.db.hibernate.*;
+import de.fhg.fokus.hss.auth.HexCodec;
+import de.fhg.fokus.hss.cx.CxConstants;
+import de.fhg.fokus.hss.db.model.IMPU;
+import de.fhg.fokus.hss.db.model.CxEvents;
+import de.fhg.fokus.hss.db.op.IMPI_IMPU_DAO;
+import de.fhg.fokus.hss.db.op.IMPU_DAO;
+import de.fhg.fokus.hss.db.op.CxEvents_DAO;
+import de.fhg.fokus.hss.web.form.IMPI_Form;
+import de.fhg.fokus.hss.web.util.WebConstants;
+import de.fhg.fokus.hss.zh.ZhConstants;
 
 class AddSubsFHoSS {
     private static Logger logger = Logger.getLogger(AddSubsFHoSS.class);
     public static void main(String[] args) {
         System.out.println("Adding Subscribers to FHoSS ..."); 
+        int imsu_id = createIMSU(args);
+        int ipmi_id = createIMPI(args);
+        int impu_id = createIMPU(args);
+        System.out.println("Added Subscriber: " + args[0]); 
+    }
+    public static int createIMSU(String[] args) {
+        int id = 0;
         boolean dbException = false;
         try{
             Session session = HibernateUtil.getCurrentSession();
@@ -25,8 +42,8 @@ class AddSubsFHoSS {
             imsu.setId_capabilities_set(Integer.parseInt(args[1]));
             imsu.setId_preferred_scscf_set(Integer.parseInt(args[2]));
             IMSU_DAO.insert(session, imsu);
-            Integer id = imsu.getId();
-            System.out.println("Added Subscriber to FHoSS with id: " + id); 
+            id = imsu.getId();
+            System.out.println("Added IMSU: " + args[0]); 
         }                
         catch (HibernateException e){
             logger.error("Hibernate Exception occured!\nReason:" + e.getMessage());
@@ -39,6 +56,96 @@ class AddSubsFHoSS {
             }
             HibernateUtil.closeSession();
         }
+        return id;
+    }
+
+    public static int createIMPI(String[] args) {
+        int id = 0;
+        boolean dbException = false;
+        try{
+            Session session = HibernateUtil.getCurrentSession();
+            HibernateUtil.beginTransaction();
+            int auth_scheme = 255;
+            IMPI impi;
+            String identity = args[0] + "@ims.mnc001.mcc001.3ppnetwork.org";
+            String secretKey = args[3];
+            String amf = args[4];
+            String op = args[5];
+            String opc = args[6];
+            impi = new IMPI();
+            impi.setZh_default_auth_scheme(CxConstants.Auth_Scheme_AKAv1);
+            impi.setZh_key_life_time(ZhConstants.Default_Key_Life_Time);
+            impi.setZh_uicc_type(ZhConstants.UICC_Type_Basic_GBA);
+            impi.setIdentity(identity);
+            if (secretKey.length()==32) 
+                impi.setK(HexCodec.decode(secretKey));
+            else
+                impi.setK(secretKey.getBytes());
+            impi.setAuth_scheme(auth_scheme);
+            impi.setAmf(HexCodec.decode(amf));
+            impi.setOp(HexCodec.decode(op));
+            impi.setOpc(HexCodec.decode(opc));
+            IMPI_DAO.insert(session, impi);
+            id = impi.getId();
+            System.out.println("Added IMPI: " + args[0]); 
+        }                
+        catch (HibernateException e){
+            logger.error("Hibernate Exception occured!\nReason:" + e.getMessage());
+            e.printStackTrace();
+            dbException = true;
+        }
+        finally{
+            if (!dbException){
+                HibernateUtil.commitTransaction();
+            }
+            HibernateUtil.closeSession();
+        }
+        return id;
+    }
+
+    public static int createIMPU(String[] args) {
+        return 0;
+        int id = 0;
+        boolean dbException = false;
+        try{
+            Session session = HibernateUtil.getCurrentSession();
+            HibernateUtil.beginTransaction();
+            int auth_scheme = 255;
+            IMPI impi;
+            String identity = args[0] + "@ims.mnc001.mcc001.3ppnetwork.org";
+            String secretKey = args[3];
+            String amf = args[4];
+            String op = args[5];
+            String opc = args[6];
+            impi = new IMPI();
+            impi.setZh_default_auth_scheme(CxConstants.Auth_Scheme_AKAv1);
+            impi.setZh_key_life_time(ZhConstants.Default_Key_Life_Time);
+            impi.setZh_uicc_type(ZhConstants.UICC_Type_Basic_GBA);
+            impi.setIdentity(identity);
+            if (secretKey.length()==32) 
+                impi.setK(HexCodec.decode(secretKey));
+            else
+                impi.setK(secretKey.getBytes());
+            impi.setAuth_scheme(auth_scheme);
+            impi.setAmf(HexCodec.decode(amf));
+            impi.setOp(HexCodec.decode(op));
+            impi.setOpc(HexCodec.decode(opc));
+            IMPI_DAO.insert(session, impi);
+            id = impi.getId();
+            System.out.println("Added IMPI: " + args[0]); 
+        }                
+        catch (HibernateException e){
+            logger.error("Hibernate Exception occured!\nReason:" + e.getMessage());
+            e.printStackTrace();
+            dbException = true;
+        }
+        finally{
+            if (!dbException){
+                HibernateUtil.commitTransaction();
+            }
+            HibernateUtil.closeSession();
+        }
+        return id;
     }
 }
 
